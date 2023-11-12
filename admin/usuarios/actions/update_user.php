0bfbab586ce1bf85bd3b7bd233573cb86d69b63f
Angel Alemany <angelemegeele@gmail.com>
2023-11-12 15:03:44 -0300

    feat: update user form

---
+++ admin/usuarios/actions/update_user.php
@@ -0,0 +1,70 @@
+<?php
+
+require '../../../config/config.php';
+require '../../../helpers/forms.php';
+
+// iniciar sesión y verificar autorización
+session_start();
+
+if ($_SESSION['role'] !== 'superadmin') {
+    header("Location: " . BASE_URL . "/login.php");
+    exit;
+}
+
+// Recibe los datos del formulario.
+$user_id = $_POST['user_id']; // Asegúrate de enviar el ID del usuario desde el formulario.
+$email = $_POST['email'];
+$full_name = $_POST['full_name'];
+$username = $_POST['username'];
+$is_active = isset($_POST['is_active']) ? 1 : 0;
+$old_photo = $_POST['old_photo'];
+
+$upload_system_dir = "../../../uploads/users/";
+$upload_url_dir = "/uploads/users/";
+
+// Manejar la subida de la foto de perfil
+try {
+    if (isset($_FILES['profile_picture'])) {
+        $profile_picture_path = upload_photo($_FILES['profile_picture'], $upload_system_dir, $upload_url_dir);
+    }
+} catch (Exception $e) {
+    handle_form_error($e->getMessage(), $_POST, "/admin/usuarios/edit.php?username=" . $username);
+}
+
+$profile_picture_path = $profile_picture_path ? $profile_picture_path : $old_photo;
+
+// Conexión a la base de datos y preparación de la consulta.
+$stmt = $mydb->prepare("
+    UPDATE users SET email = ?, full_name = ?, username = ?, profile_picture = ?, is_active = ?
+    WHERE id = ?
+");
+
+$stmt->bind_param("ssssii", $email, $full_name, $username, $profile_picture_path, $is_active, $user_id);
+
+try {
+    if ($stmt->execute()) {
+        $_SESSION['success'] = "Usuario actualizado exitosamente";
+
+        // Cerrar la sentencia y la conexión antes de redirigir
+        $stmt->close();
+        $mydb->close();
+
+        header("Location: " . BASE_URL . "/admin/usuarios");
+        exit;
+    } else {
+        handle_form_error("Error: " . $stmt->error, array(
+            'email' => $email,
+            'full_name' => $full_name,
+            'username' => $username,
+        ), "/admin/usuarios/edit.php?username=" . $username);
+    }
+} catch (Exception $e) {
+    handle_form_error("Error: " . $e->getMessage(), array(
+        'email' => $email,
+        'full_name' => $full_name,
+        'username' => $username,
+    ), "/admin/usuarios/edit.php?username=" . $username);
+}
+
+$stmt->close();
+$mydb->close();
---
+++ admin/usuarios/edit.php
@@ -0,0 +1,104 @@
+<?php
+
+require '../../config/config.php';
+require '../../helpers/forms.php';
+require '../../helpers/users.php';
+
+// iniciar sesión y verificar autorización
+session_start();
+
+if ($_SESSION['role'] !== 'superadmin') {
+    header("Location: " . BASE_URL . "/login.php");
+    exit;
+}
+
+$username = $_GET["username"];
+
+$user = null;
+
+// preparar la consulta
+$stmt = $mydb->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+
+// ejecutar la consulta
+$stmt->execute();
+
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+}
+
+$stmt->close();
+$mydb->close();
+
+// Si no se encontró al usuario, redirige a la página de lista de usuarios.
+if ($user === null) {
+    header("Location: " . BASE_URL . "/admin/usuarios.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Agregar usuario</title>
+    <?php include '../../components/meta.php'; ?>
+</head>
+
+<body>
+    <?php include '../../components/admin/header.php'; ?>
+    <main class="container py px">
+        <div class="admin-bar">
+            <h1>Agregar usuario</h1>
+            <a class="btn btn-secondary" href="<?php echo BASE_URL ?>/admin/usuarios">Regresar</a>
+        </div>
+
+        <section>
+            <?php
+            if (isset($_SESSION['error'])) {
+                echo '<p class="error">';
+                echo $_SESSION['error'];
+                echo '</p>';
+                unset($_SESSION['error']);
+            }
+            ?>
+            <form class="custom-form" action="./actions/update_user.php" method="POST" enctype="multipart/form-data">
+                <input type="hidden" id="user_id" name="user_id" value="<?php echo $user['id']; ?>">
+                <input type="hidden" id="old_photo" name="old_photo" value="<?php echo $user['profile_picture']; ?>">
+
+                <div class="data-section">
+                    <div class="input-wrapper text-input">
+                        <label for="full_name">Nombre completo: <span class="required">*</span></label>
+                        <input type="text" id="full_name" name="full_name" value="<?php echo $user['full_name']; ?>" required>
+                    </div>
+
+                    <div class="input-wrapper text-input">
+                        <label for="email">Email: <span class="required">*</span></label>
+                        <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
+                    </div>
+
+                    <div class="input-wrapper text-input">
+                        <label for="username">Nombre de usuario: <span class="required">*</span></label>
+                        <input type="text" id="username" name="username" value="<?php echo $user['username']; ?>" required>
+                    </div>
+                </div>
+
+                <div class="manage-section">
+                    <div class="input-wrapper checkbox-input">
+                        <label for="is_active">Activo:</label>
+                        <input type="checkbox" id="is_active" name="is_active" <?php echo ($user['is_active'] == 1) ? 'checked' : ''; ?>>
+                    </div>
+
+                    <?php include '../../components/admin/profile_picture_field.php' ?>
+
+                    <input id="submit-btn" class="btn btn-primary" type="submit" value="Actualizar Usuario">
+                </div>
+            </form>
+            <?php unset($_SESSION['form_data']); ?>
+        </section>
+    </main>
+    <script src="<?php echo BASE_URL ?>/assets/js/users.js"></script>
+</body>
+
+</html>
