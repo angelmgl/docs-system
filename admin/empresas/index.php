954d910c56742837fe70fae2bdbe45108073bf2f
Angel Alemany <angelemegeele@gmail.com>
2023-11-12 11:34:18 -0300

    feat: add list businesses

--- admin/empresas/index.php
+++ admin/empresas/index.php
@@ -10,6 +10,20 @@ if ($_SESSION['role'] !== 'superadmin') {
     exit;
 }
 
+$sql = "SELECT * FROM businesses";
+$stmt = $mydb->prepare("SELECT * FROM businesses");
+$stmt->execute();
+$result = $stmt->get_result();
+
+$businesses = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $businesses[] = $row;
+    }
+}
+
+$mydb->close();
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +37,31 @@ if ($_SESSION['role'] !== 'superadmin') {
 <body>
     <?php include '../../components/admin/header.php' ?>
     <section class="container py px">
-        <h1>Administrar Empresas</h1>
+        <div class="admin-bar">
+            <h1>Administrar Empresas</h1>
+            <a class="btn btn-primary" href="<?php echo BASE_URL ?>/admin/empresas/add.php">Añadir empresa</a>
+        </div>
+
+        <?php
+        if (isset($_SESSION['success'])) {
+            echo '<p class="success">';
+            echo $_SESSION['success'];
+            echo '</p>';
+            unset($_SESSION['success']);
+        }
+        ?>
+
+        <?php if (empty($businesses)) { ?>
+            <p>No hay resultados para esta búsqueda...</p>
+        <?php } else { ?>
+            <ul>
+                <?php
+                foreach ($businesses as $business) { ?>
+                    <li><?php echo $business["name"] ?></li>
+                <?php }
+                ?>
+            </ul>
+        <?php } ?>
     </section>
 </body>
 
