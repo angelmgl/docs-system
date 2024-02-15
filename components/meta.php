4d77f44a30a07c8b1411f82b05c920008ba9ee54
Angel Alemany <angelemegeele@gmail.com>
2024-02-15 13:46:37 -0300

    feat: add styling

--- components/meta.php
+++ components/meta.php
@@ -4,3 +4,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+<link rel="icon" href="https://analytico.com.py/wp-content/uploads/2023/11/cropped-icono_analytico-32x32.png" sizes="32x32">
+<link rel="icon" href="https://analytico.com.py/wp-content/uploads/2023/11/cropped-icono_analytico-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="https://analytico.com.py/wp-content/uploads/2023/11/cropped-icono_analytico-180x180.png">
+<meta name="msapplication-TileImage" content="https://analytico.com.py/wp-content/uploads/2023/11/cropped-icono_analytico-270x270.png">
